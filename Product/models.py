cdfb4b8b18ba1511c882b36a3ae9588250b7f1cd
sergei <you@example.com>
2023-11-29 22:23:51 +0800

    Initial commit

---
+++ Product/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from Category.models import Category
+
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', null=True, verbose_name='Изображение (превью)')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за штуку')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
