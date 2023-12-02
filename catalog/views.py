b547f30520c994eb83b8870424c5b24a26f74090
sergei <you@example.com>
2023-12-02 22:57:56 +0800

    homework 3

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from Product.models import Product
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/home.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'SkyStore'
+    }
+    return render(request, 'main/home.html', context)
 
 
 def contacts(request):
@@ -14,4 +21,12 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name}, {phone}, {message}')
 
-    return render(request, 'main/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, 'main/contacts.html', context)
+
+
+def products(request):
+    return render(request, 'main/products.html')
